de8a05c032ccfa7b3a11931e51b463093367ff55
Gaganyadav2209 <98753163+Gaganyadav2209@users.noreply.github.com>
2022-08-18 23:23:32 +0530

    Create pre_computation.cpp

---
+++ pre_computation.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const int M = 1e9 + 7;
+const int N = 1e5 + 10;
+long long fact[N];
+
+
+
+int main() {
+
+
+
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+
+	freopen("input.txt", "r", stdin);
+
+	freopen("output.txt", "w", stdout);
+
+#endif
+
+
+	fact[0] = fact[1] = 1;
+	for (int i = 2; i < N; i++) {
+		fact[i] = fact[i - 1] * i;
+	}
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << fact[n] << endl;
+
+	}
+
+
+	return 0;
+}
